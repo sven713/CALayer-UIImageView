9a761d0dca9adf9ffb8706ffb36548fd890dc156
songximing <songximing@wtoip.com>
2018-06-29 11:29:29 +0800

    CALayer替代UIImageView显示

---
+++ CALayer-UIImageView/CALayer-UIImageView/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  CALayer-UIImageView
+//
+//  Created by sve on 2018/6/29.
+//  Copyright © 2018年 sve. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self layer_ImageVeiw];
+}
+
+- (void)layer_ImageVeiw {
+    CALayer *layer = [[CALayer alloc] init];
+    [self.view.layer addSublayer:layer];
+    
+    layer.contents = (id)[UIImage imageNamed:@"把时间当做朋友"].CGImage; // 必须转换成CGImage否则显示不出来
+    layer.frame = CGRectMake(20, 100, 300, 300);
+    
+    // layer不能添加手势
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(aaaa)];
+    
+}
+
+@end
